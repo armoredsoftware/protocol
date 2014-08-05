501ae11b6ab2dbdb2c7ef63502dcc6939c1c12ff
Paul Kline <pauliankline@gmail.com>
2014-08-05 10:19:03 -0500

    minor edits

--- demos/demo2/Demo2Shared.hs
+++ demos/demo2/Demo2Shared.hs
@@ -131,9 +131,9 @@ ePack e n = (ePack' e) `B.append` (B.pack n) --pik
 -- if it is something else.  see comment below
 ePack' :: Evidence -> B.ByteString
 ePack' es = foldr f B.empty (evidencePieceList es)
-  where f (Demo2Shared.M0 x) y = x `B.append` (B.pack y) -- (i.e. (toByteString x) `append` y )
-        f (Demo2Shared.M1 x) y = x `B.append` (B.pack y)
-        f (Demo2Shared.M2 x) y = x `B.append` (B.pack y)
+  where f (Demo2Shared.M0 x) y =(B.pack x) `B.append` (B.pack y) -- (i.e. (toByteString x) `append` y )
+        f (Demo2Shared.M1 x) y = (B.pack x) `B.append` (B.pack y)
+        f (Demo2Shared.M2 x) y =(B.pack x) `B.append` (B.pack y)
 
 qPack :: Quote -> Hash -> B.ByteString
 qPack (Quote q) hash = 
