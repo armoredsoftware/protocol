b7ed9cf109e337afcc4836e2510bb3efecc13699
Paul Kline <pauliankline@gmail.com>
2014-08-05 16:37:28 -0500

    commit_to_push_toVMs

--- demos/demo2/JSONCaster.hs
+++ demos/demo2/JSONCaster.hs
@@ -14,6 +14,7 @@ import qualified Data.ByteString as B
 
 
 $(deriveJSON defaultOptions ''EvidenceDescriptor)
+$(deriveJSON defaultOptions ''EvidenceDescriptorW)
 $(deriveJSON defaultOptions ''DesiredEvidence)
 $(deriveJSON defaultOptions ''EvidencePiece)
 $(deriveJSON defaultOptions ''Quote)
