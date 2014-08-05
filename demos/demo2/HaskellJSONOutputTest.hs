8f730985a3365815f8f0baaa681068ce47c60a1a
Paul Kline <pauliankline@gmail.com>
2014-08-05 17:38:04 -0500

    added haskell file to output the json forms

---
+++ demos/demo2/HaskellJSONOutputTest.hs
@@ -0,0 +1,17 @@
+module HaskellJSONOutputTest where
+
+import JSONCaster
+import Demo2Shared
+import Data.Bits
+import System.Environment
+
+evidenceDescriptorW = EDW D1
+desiredEvidence = DesiredEvidence [D0,D1,D2]
+evidencePiece = = M0 [bit 1]
+
+
+
+main :: IO ()
+main = do
+  writeFile "./testoutput" "test stuff goes here!!"
+  
