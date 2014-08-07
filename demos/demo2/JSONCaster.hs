1f48c81ff3047859824731a1e1c222f5c8cb50b0
Paul Kline <pauliankline@gmail.com>
2014-08-07 18:01:27 -0500

    I went too far. Revert "updated_named_fields"
    This reverts commit 2bc39f8b67fbd0ed5a56fa31e94d88b26d07d789.

--- demos/demo2/JSONCaster.hs
+++ demos/demo2/JSONCaster.hs
@@ -13,35 +13,25 @@ import qualified Data.ByteString.Lazy.Char8 as LBC
 import qualified Data.ByteString as B
 
 --data EvidenceDescriptorW = EDW {evidenceDescriptor :: EvidenceDescriptor} deriving ( Show)
-{--data WrappedData = EvidenceDescriptorW {getEvidenceDescriptor :: EvidenceDescriptor} |
-                   DesiredEvidenceW {desiredEvidence :: DesiredEvidence} |
-                   EvidencePieceW {evidencePiece :: EvidencePiece} |
-                   QuoteW {quote :: Quote} |
-                   EvidenceW {evidence :: Evidence} |
-                   RequestW {request :: Request} |
-                   ResponseW {response :: Response} |
-                   EvidencePackageW {evidencePackage :: EvidencePackage} |
-                   QuotePackageW {quotePackage :: QuotePackage} deriving (Show
---}
 
-data WrappedData = EvidenceDescriptorW EvidenceDescriptor
-                 | DesiredEvidenceW DesiredEvidence
-                 | EvidencePieceW  EvidencePiece
-                 | QuoteW  Quote
-                 | EvidenceW  Evidence
-                 | RequestW  Request
-                 | ResponseW Response
-                 | EvidencePackageW EvidencePackage
-                 | QuotePackageW QuotePackage deriving (Show)
+data WrappedData = EvidenceDescriptorW {getEvidenceDescriptor :: EvidenceDescriptor} |
+                   DesiredEvidenceW {getDesiredEvidence :: DesiredEvidence} |
+                   EvidencePieceW {getEvidencePiece :: EvidencePiece} |
+                   QuoteW {getQuote :: Quote} |
+                   EvidenceW {getEvidence :: Evidence} |
+                   RequestW {getRequest :: Request} |
+                   ResponseW {getResponse :: Response} |
+                   EvidencePackageW {getEvidencePackage :: EvidencePackage} |
+                   QuotePackageW {getQuotePackage :: QuotePackage} deriving (Show)
 $(deriveJSON defaultOptions ''WrappedData)
 
 
 $(deriveJSON defaultOptions ''EvidenceDescriptor)
 --(deriveJSON defaultOptions ''EvidenceDescriptorW)
---(deriveJSON defaultOptions ''DesiredEvidence)
+$(deriveJSON defaultOptions ''DesiredEvidence)
 $(deriveJSON defaultOptions ''EvidencePiece)
 $(deriveJSON defaultOptions ''Quote)
---(deriveJSON defaultOptions ''Evidence)
+$(deriveJSON defaultOptions ''Evidence)
 $(deriveJSON defaultOptions ''Request)
 $(deriveJSON defaultOptions ''Response)
 $(deriveJSON defaultOptions ''EvidencePackage)
