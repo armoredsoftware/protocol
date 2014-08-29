233c3a60a48c5739464087f0ebf193d216a28ddc
Adam Petz <apetz@g.emporia.edu>
2014-08-29 14:21:47 -0500

    changed back quote_info encoding

--- tpm/mainline/src/TPM/Types.hs
+++ tpm/mainline/src/TPM/Types.hs
@@ -407,11 +407,11 @@ data TPM_QUOTE_INFO = TPM_QUOTE_INFO {
 instance Binary TPM_QUOTE_INFO where
     put (TPM_QUOTE_INFO c d) = do
         put tpm_struct_ver_default
-        put (0x51 :: Word8)
-        put (0x55 :: Word8)
-        put (0x4F :: Word8)
-        put (0x54 :: Word8)
-        --put $ C.pack "QUOT"
+        --put (0x51 :: Word8)
+        --put (0x55 :: Word8)
+        --put (0x4F :: Word8)
+        --put (0x54 :: Word8)
+        put $ C.pack "QUOT"
         put c
         put d
     get = do
