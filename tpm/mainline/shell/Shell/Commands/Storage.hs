e1bd803acae4a6b30311d0cf77cbe976423ef2fd
Adam Petz <apetz@g.emporia.edu>
2014-08-30 13:35:43 -0500

    change encoded message

--- tpm/mainline/shell/Shell/Commands/Storage.hs
+++ tpm/mainline/shell/Shell/Commands/Storage.hs
@@ -154,7 +154,7 @@ cmd_key = ShellCmd ["key","k"]
 
             let (pub, pri) = getKeyPair
             qi <- liftIO $ makeQuoteInfo tpm
-            let qiBlob = encode qi
+            let qiBlob = encode "hello"
                 qiSig = rsassa_pkcs1_v1_5_sign ha_SHA1 pri qiBlob
             case rsassa_pkcs1_v1_5_verify ha_SHA1 pub qiBlob qiSig of
               True -> liftIO $ putStrLn "VERIFIED!!!"
