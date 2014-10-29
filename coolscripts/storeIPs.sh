53def79fdf054c04c5f0dfa983bf813457c12a78
paul-kline <pauliankline@gmail.com>
2014-10-29 17:23:48 -0500

    added helpful scripts and readme

--- coolscripts/storeIPs.sh
+++ coolscripts/storeIPs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #IMPORTANT execute 'updatedb' in the terminal if this does not locate correctly
-OUTPUT="$(locate -n 1 coolscripts/getVMIp.exp)"
-$OUTPUT > ips.txt
+#getvmi="$(locate -n 1 coolscripts/getVMIp.exp)"
+mypath="$(locate -n 1 /coolscripts)"
+$mypath/getVMIp.exp > $mypath/ips.txt
+#$getvmi > ips.txt
 #./getVMIp.exp > ips.txt
 #/home/armored/scp.sh
 
