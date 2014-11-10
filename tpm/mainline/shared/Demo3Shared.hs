9fcdfbfb8bebc89baffcb64b533e3fd287545c21
paul-kline <pauliankline@gmail.com>
2014-11-10 15:10:42 -0600

    umm.. nothing changed that I recall. Doing this to merge.

---
+++ tpm/mainline/shared/Demo3Shared.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverlappingInstances, OverloadedStrings, RecordWildCards  #-}
+
+module Demo3Shared (module Demo3SharedNOVCHAN,
+		    module Demo3Vchan) where
+
+import Demo3SharedNOVCHAN
+import Demo3Vchan
