1b9db38d82d900f196820fe47cfe03848e0d42b2
Jason Gevargizian <jgevargizian@gmail.com>
2014-11-17 02:27:33 -0600

    Adding hotspot (the measurer).

---
+++ measurer/hotspot/src/share/vm/runtime/TalkThread.cpp
@@ -0,0 +1,155 @@
+#include "precompiled.hpp"
+#include "runtime/task.hpp"
+#include "runtime/vmThread.hpp"
+#include "runtime/timer.hpp"
+#include "runtime/java.hpp"
+#include "runtime/vm_operations.hpp"
+#include "oops/methodOop.hpp"
+
+#include "runtime/TalkThread.hpp"
+
+//sockets
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h> 
+#include <string>
+
+
+TalkThread* TalkThread::_talk_thread   = NULL;
+volatile bool  TalkThread::_should_terminate = false;
+
+
+TalkThread::TalkThread() : Thread() 
+{
+  assert(talk_thread() == NULL, "we can only allocate one TalkThread");
+  if (os::create_thread(this, os::talk_thread)) {
+    _talk_thread = this;
+
+    //CHECK: we probably want this here, right?
+    // Set the talk thread to the highest OS priority which should not be
+    // used, unless a Java thread with priority java.lang.Thread.MAX_PRIORITY
+    // is created. The only normal thread using this priority is the reference
+    // handler thread, which runs for very short intervals only.
+    // If the VMThread's priority is not lower than the TalkThread profiling
+    // will be inaccurate.
+    //EDIT changed this to NormPriority rather than max
+    os::set_priority(this, MaxPriority);
+    if (!DisableStartThread) {
+      os::start_thread(this);
+    }
+  }
+}
+
+
+void TalkThread::run() {
+	//Leave this blank for now
+	printf("ENTRY: TalkThread::run()\n");
+
+	int sockfd = 0, n = 0;
+	char recvBuff[1024];
+    	char sendBuff[1025];
+	struct sockaddr_in serv_addr; 
+
+	memset(recvBuff, '0',sizeof(recvBuff));
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Error : Could not create socket \n");		
+	} 
+
+		memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_port = htons(5000); 
+
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+	{
+		printf("\n inet_pton error occured\n");
+	} 
+
+	if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("\n Error : Connect Failed \n");
+	} 
+
+
+	while(1){
+
+		//GET REQUEST
+	 	printf("\n TalkThread : Awaiting request\n");
+		n=0;
+
+		n = read(sockfd, recvBuff, sizeof(recvBuff)-1);
+		recvBuff[n] = 0;
+		if(fputs(recvBuff, stdout) == EOF)
+		{
+		    printf("\n Error : Fputs error\n");
+		} 
+
+		if(n < 0)
+		{
+			printf("\n Read error \n");
+			return;
+		} 
+
+		//SEND RESULT
+
+	 	printf("\n TalkThread : Sending result\n");
+		
+		snprintf(sendBuff, sizeof(sendBuff), "THERESULT\n");
+        	write(sockfd, sendBuff, strlen(sendBuff)); 
+		
+
+	}
+
+	while(1){
+		printf("tick!\n");
+		sleep(5);	
+	}
+}
+
+void TalkThread::start() {
+  printf("ENTRY: TalkThread::start()\n");
+  if (talk_thread() == NULL) {
+    _should_terminate = false;
+    // Create the single instance of WatcherThread
+    new TalkThread();
+  }
+}
+
+void TalkThread::stop() {
+  printf("ENTRY: TalkThread::stop()\n");
+
+  // it is ok to take late safepoints here, if needed
+  MutexLocker mu(Terminator_lock);
+  _should_terminate = true;
+  OrderAccess::fence();  // ensure WatcherThread sees update in main loop
+
+
+  Thread* talk = talk_thread();
+  if (talk != NULL)
+    talk->_SleepEvent->unpark();
+
+  while(talk_thread() != NULL) {
+    // This wait should make safepoint checks, wait without a timeout,
+    // and wait as a suspend-equivalent condition.
+    //
+    // Note: If the FlatProfiler is running, then this thread is waiting
+    // for the WatcherThread to terminate and the WatcherThread, via the
+    // FlatProfiler task, is waiting for the external suspend request on
+    // this thread to complete. wait_for_ext_suspend_completion() will
+    // eventually timeout, but that takes time. Making this wait a
+    // suspend-equivalent condition solves that timeout problem.
+    //
+    Terminator_lock->wait(!Mutex::_no_safepoint_check_flag, 0,
+                          Mutex::_as_suspend_equivalent_flag);
+  }
+}
+
+void TalkThread::print_on(outputStream* st) const {
+  st->print("\"%s\" ", name());
+  Thread::print_on(st);
+  st->cr();
+}
---
+++ measurer/hotspot/src/share/vm/runtime/TalkThread.hpp
@@ -0,0 +1,36 @@
+#include "runtime/task.hpp"
+#include "runtime/timer.hpp"
+#include "memory/allocation.hpp"
+
+class TalkThread: public Thread {
+  friend class VMStructs;
+ public:
+  virtual void run();
+
+ private:
+  static TalkThread* _talk_thread;
+
+  volatile static bool _should_terminate; // updated without holding lock
+ public:
+  enum SomeConstants {
+    delay_interval = 10                          // interrupt delay in milliseconds
+    };
+
+  // Constructor
+  TalkThread();
+
+  // Tester
+  bool is_Talk_thread() const                 { return true; }
+
+  // Printing
+  char* name() const { return (char*)"VM Talk Thread"; }
+  void print_on(outputStream* st) const;
+  void print() const { print_on(tty); }
+
+  // Returns the single instance of TalkThread
+  static TalkThread* talk_thread()         { return _talk_thread; }
+
+  // Create and start the single instance of TalkThread, or stop it on shutdown
+  static void start();
+  static void stop();
+  };
