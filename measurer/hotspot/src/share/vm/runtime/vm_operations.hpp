b1c670de66a838779ac17efb872deab22187a334
Jason Gevargizian <jgevargi@jello.ittc.ku.edu>
2014-12-18 13:48:13 -0600

    Merge all of PAPI changes

--- measurer/hotspot/src/share/vm/runtime/vm_operations.hpp
+++ measurer/hotspot/src/share/vm/runtime/vm_operations.hpp
@@ -96,7 +96,7 @@
   template(HeapWalkOperation)                     \
   template(HeapIterateOperation)                  \
   template(ReportJavaOutOfMemory)                 \
-  template(JRMethodCountTraceVMOP) \*JG - Change Line*\       \
+  template(JRMethodCountTraceVMOP)			      \
   template(JRStackWatcherDeopt)                   \
   template(Exit)                                  \
 
