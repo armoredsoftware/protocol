25bdcf2f59744cc14ff6be93911bbe3532ef85de
ampetz <ampetz@ku.edu>
2015-07-01 12:59:35 -0500

    Removed editline source dependency from sandbox

--- tpm/mainline/sandboxinit.sh
+++ tpm/mainline/sandboxinit.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cabal sandbox init
 cabal sandbox add-source ../rsa
-cabal sandbox add-source ../editline/editline-0.2.1.1/
 cabal sandbox add-source ../../../../codeLocal/code/experiments/vchanTestCentOS_1/haskell/VChanUtil/
-cabal sandbox add-source ../shell/mainline/
